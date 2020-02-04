036aecf0121e203d6dc9fc762998fd9ca249894d
Xuefeng Zhu <xzhu15@illinois.edu>
2020-02-04 12:50:52 -0800

    Updates

---
+++ precache-manifest.6bff389486e07e1cb44112c41a0d9d20.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c6a0692e7bea1379c4b5e020a28738",
+    "url": "/sgn-contract/index.html"
+  },
+  {
+    "revision": "4299de52c2c231bdb927",
+    "url": "/sgn-contract/static/css/3.46439c26.chunk.css"
+  },
+  {
+    "revision": "6a08eb3bdb39cc0f74ce",
+    "url": "/sgn-contract/static/css/main.cde22e43.chunk.css"
+  },
+  {
+    "revision": "f1571c655d03fc692462",
+    "url": "/sgn-contract/static/js/0.d04b3cfe.chunk.js"
+  },
+  {
+    "revision": "4299de52c2c231bdb927",
+    "url": "/sgn-contract/static/js/3.12d2d184.chunk.js"
+  },
+  {
+    "revision": "afa1151daca100497901",
+    "url": "/sgn-contract/static/js/4.fc97d929.chunk.js"
+  },
+  {
+    "revision": "3c1b6b8c6a6099329e93",
+    "url": "/sgn-contract/static/js/5.565dfa35.chunk.js"
+  },
+  {
+    "revision": "5233fde2e71670e122dd",
+    "url": "/sgn-contract/static/js/6.6e7ef79d.chunk.js"
+  },
+  {
+    "revision": "58eb0e05c4d4f5ea8efc",
+    "url": "/sgn-contract/static/js/7.caed67bb.chunk.js"
+  },
+  {
+    "revision": "6a08eb3bdb39cc0f74ce",
+    "url": "/sgn-contract/static/js/main.e8ef56a6.chunk.js"
+  },
+  {
+    "revision": "1360ee3e62654869989d",
+    "url": "/sgn-contract/static/js/runtime~main.73a88369.js"
+  }
+]);
