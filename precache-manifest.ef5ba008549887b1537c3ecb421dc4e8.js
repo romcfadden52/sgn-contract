3f1ebee09e3b86be2ca2ebccd78b1d50cc2af2ad
Ubuntu <ubuntu@ip-172-26-0-15.us-west-2.compute.internal>
2020-09-01 15:31:07 +0000

    Updates

---
+++ precache-manifest.ef5ba008549887b1537c3ecb421dc4e8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4027ae95c51fdbbb77454484cf0a713",
+    "url": "/sgn-contract/index.html"
+  },
+  {
+    "revision": "6a4742553433fe377741",
+    "url": "/sgn-contract/static/css/4.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "5491c9a464dd644727da",
+    "url": "/sgn-contract/static/css/main.4f79e1df.chunk.css"
+  },
+  {
+    "revision": "b286971dc6565a338574",
+    "url": "/sgn-contract/static/js/0.caf2443a.chunk.js"
+  },
+  {
+    "revision": "9c1cb0eadddb823f4c07",
+    "url": "/sgn-contract/static/js/1.7bd07c38.chunk.js"
+  },
+  {
+    "revision": "81941f68c1a998423e9c",
+    "url": "/sgn-contract/static/js/10.600127ca.chunk.js"
+  },
+  {
+    "revision": "6a4742553433fe377741",
+    "url": "/sgn-contract/static/js/4.de49d75e.chunk.js"
+  },
+  {
+    "revision": "00f6b3a2b5d7b20bf5ff0f91e8bc92dc",
+    "url": "/sgn-contract/static/js/4.de49d75e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595d4879bfa598636a9e",
+    "url": "/sgn-contract/static/js/5.27209fa0.chunk.js"
+  },
+  {
+    "revision": "a77dd4d713b908035bb3",
+    "url": "/sgn-contract/static/js/6.dc2a6987.chunk.js"
+  },
+  {
+    "revision": "c8be8e6bf9eca28ac765",
+    "url": "/sgn-contract/static/js/7.a6eb7e89.chunk.js"
+  },
+  {
+    "revision": "7abd28321305a957aa5e",
+    "url": "/sgn-contract/static/js/8.13fe6e71.chunk.js"
+  },
+  {
+    "revision": "757eb7cf0cfe84ffb17d",
+    "url": "/sgn-contract/static/js/9.8091b5b0.chunk.js"
+  },
+  {
+    "revision": "5491c9a464dd644727da",
+    "url": "/sgn-contract/static/js/main.1b72d29d.chunk.js"
+  },
+  {
+    "revision": "60feaaed03b46f330f8c",
+    "url": "/sgn-contract/static/js/runtime-main.d6a2b06f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sgn-contract/precache-manifest.e2b1e02668d96bbe33c4152e3014cac5.js"
+  "/sgn-contract/precache-manifest.ef5ba008549887b1537c3ecb421dc4e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sgn-contract/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
